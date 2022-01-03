1a48c736344849873cc821a78eff82b0f06a7424
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-03 13:31:48 -0800

    added decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundfn = originalMethod.bind(this);
+            return boundfn;
+        },
+    };
+    return adjDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -33,13 +45,14 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener("submit", this.submitHandler.bind(this));
+        this.element.addEventListener("submit", this.submitHandler);
     }
 
     private attach() {
