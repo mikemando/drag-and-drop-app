45da556a2d227ec779c19ab0fca2c8122ea6a573
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-04 19:56:32 -0800

    rendered project list

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,7 @@ class ProjectList {
     hostElement: HTMLDivElement;
     element: HTMLElement;
 
-    constructor(private type: "active | finished") {
+    constructor(private type: "active" | "finished") {
         this.templateElement = document.getElementById(
             "project-list"
         )! as HTMLTemplateElement;
@@ -74,6 +74,19 @@ class ProjectList {
 
         this.element = importedNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul")!.id = listId;
+        this.element.querySelector("h2")!.textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
     }
 }
 
@@ -174,3 +187,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrj = new ProjectList("active");
+const finishedPrj = new ProjectList("finished");
