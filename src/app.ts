9812d27db0f211e184d9ac3c0833bc7e2a9e0425
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-09 02:51:23 -0800

    drag and drop interface

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 interface Validatable {
     value: number | string;
     required?: boolean;
@@ -158,7 +169,10 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable
+{
     private project: Project;
 
     get person() {
@@ -176,7 +190,16 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure() {}
+    @autobind
+    dragStartHandler(event: DragEvent) {}
+
+    @autobind
+    dragEndHandler(_: DragEvent) {}
+
+    configure() {
+        this.element.addEventListener("dragstart", this.dragStartHandler);
+        this.element.addEventListener("dragend", this.dragEndHandler);
+    }
 
     renderContent() {
         this.element.querySelector("h2")!.textContent = this.project.title;
