776d24297bf0b85362d320c4158db843ee59a3e8
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-11 04:11:30 -0800

    setup tsconfig.json

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,5 +1,5 @@
-import { Component } from "./base-component.js";
 import { autobind } from "../decorators/autobind.js";
+import { Component } from "./base-component.js";
 import { validate, Validatable } from "../util/validator.js";
 import { projectState } from "../state/project-state.js";
 
